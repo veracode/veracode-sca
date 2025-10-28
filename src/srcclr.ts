20d4fd7aad146d728777dacb2d92b13275b3ef42
Shipra Kumari <skumari@veracode.com>
2025-10-28 17:51:50 +0530

    Removing output from summary message

--- src/srcclr.ts
+++ src/srcclr.ts
@@ -268,7 +268,7 @@ export async function runAction (options: Options)  {
 
                 // if scan was set to fail the pipeline should fail and show a summary of the scan results
                 if ( code != null && code > 0 && (options.breakBuildOnPolicyFindings == 'true')){
-                    let summary_info = "Veraocde SCA Scan failed with exit code "+code+"\n"+output
+                    let summary_info = "Veraocde SCA Scan failed with exit code "+code+"\n"
                     core.setFailed(summary_info)
                 }
                 //run(options,core.info);
